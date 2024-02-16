734dc46bef59e7993266ba40e3e25a3dc5bbd693
alexandervcc <alex.charco.25@gmail.com>
2024-02-15 20:51:26 -0500

    Llamaindex integration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { loadBing } from "./bing";
 import { executeChatbot } from "./chatbot";
 import { loadVariables } from "./config";
+import { llamaIndex } from "./llama-index";
 import { loadOpenAIChatbot } from "./openai/openai";
+
 
 (async () => {
   await loadVariables();
   await loadOpenAIChatbot();
   await loadBing();
-  await executeChatbot();
+  // await executeChatbot();
+  await llamaIndex();
   console.log("\n\n\n\n\n----> END")
 })();
